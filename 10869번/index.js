47c1a4db226eb3220aa3ca25331857a5b9588ed5
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-02-27 02:12:04 +0900

    1-9 "사칙연산 10869번"

---
+++ 10869번/index.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input = fs.readFileSync("index.txt").toString().split(" ");
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(parseInt(A / B));
+console.log(A % B);
