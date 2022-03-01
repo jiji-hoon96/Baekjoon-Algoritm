b443bc9f1e410ba3fc5098587964d848cfad2767
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-03-01 17:11:02 +0900

    Update index.js

--- 10926번/index.js
+++ 10926번/index.js
@@ -1,5 +1,10 @@
-//1-10 "??! 10926번"
-//
+// 1-10 "??! 10926번"
+/*
+오답이유
+1. string으로 출력해야함
+2. 하지만 split(" ")을 하게되면 object로 출력됨
+3. typeof로 타입을 확인해보니 toString()과 trim()을 이용해서 출력하면 됨
+*/
 
 let fs = require("fs");
 let input = fs.readFileSync("index.txt").toString().trim();
