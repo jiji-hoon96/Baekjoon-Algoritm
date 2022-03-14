a51ffaeaff06d5e051cff7e074b496d0abcd6b4f
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-03-14 17:39:10 +0900

    8-3"분수 찾기 1193번"
    8-3 "수정"
    
    8-3 "최종수정"

---
+++ 8. 기본수학 1/1193번/index.js
@@ -0,0 +1,23 @@
+//8-3"분수 찾기 1193번"
+//문제푸는법:
+//오답이유:
+
+let input = require("fs").readFileSync("index.txt").toString().trim();
+let number = +input;
+
+let value = 1;
+
+while (true) {
+  number -= value; //number = number-value
+  if (number <= 0) {
+    number += value; //number= number+value
+    break;
+  }
+  value++;
+}
+
+if (value % 2 === 1) {
+  console.log(`${value - (number - 1)}/${1 + number - 1}`);
+} else {
+  console.log(`${1 + number - 1}/${value - (number - 1)}`);
+}
